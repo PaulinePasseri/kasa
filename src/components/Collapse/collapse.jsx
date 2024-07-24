8d048e3b0d0f62ef7e99ed9c4a20e290952977b5
Pauline Passeri <paulinecpasseri@gmail.com>
2024-07-24 16:54:37 +0200

    composant collapse

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react'
+
 export default function Collapse({ title, description }) {
+  const [isCollapsed, setIsCollapsed] = useState(true)
   return (
     <div>
-      <i class="fa-solid fa-chevron-up"></i>
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <div onClick={() => setIsCollapsed(!isCollapsed)}>
+        <h2>{title}</h2>
+        <i
+          className={`fa-solid ${isCollapsed ? 'fa-chevron-down' : 'fa-chevron-up'}`}
+        ></i>
+      </div>
+      {!isCollapsed && <p>{description}</p>}
     </div>
   )
 }
+
+/* <div className="collapse-container">
+  <div onClick={() => setIsCollapsed(!isCollapsed)} className="collapse-header">
+    <h2>{title}</h2>
+    <i
+      className={`fa-solid fa-chevron-down ${isCollapsed ? '' : 'rotate'}`}
+    ></i>
+  </div>
+  <div className={`collapse-content ${isCollapsed ? 'collapsed' : ''}`}>
+    <p>{description}</p>
+  </div>
+</div> */
