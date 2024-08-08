b13d6c90ce76e4c949a8976be86a4d65b6582848
Pauline Passeri <paulinecpasseri@gmail.com>
2024-08-08 17:36:02 +0200

    récupération des données via fetch

--- src/components/accomodation.jsx
+++ src/components/accomodation.jsx
@@ -1,16 +1,29 @@
 import { useParams } from 'react-router-dom'
-import data from '../data.json'
 import Collapse from './collapse'
 import Slideshow from './slideshow'
 import Error from './error'
+import { useState, useEffect } from 'react'
 
 export default function Accomodation() {
   const { id } = useParams()
-  const item = data.find((item) => item.id === id)
-  const range = [1, 2, 3, 4, 5]
+  const [item, setItem] = useState(null)
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((data) => {
+        const foundItem = data.find((item) => item.id === id)
+        setItem(foundItem)
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des données:', error)
+        setItem(null)
+      })
+  }, [id])
   if (!item) {
     return <Error />
   }
+  const range = [1, 2, 3, 4, 5]
   return (
     <div className="accomodation__container">
       <Slideshow pictures={item.pictures} />
@@ -26,7 +39,7 @@ export default function Accomodation() {
             </ul>
           </div>
         </div>
-        <div className='info__container--profile'>
+        <div className="info__container--profile">
           <div className="info__container--host">
             <p>{item.host.name}</p>
             <img src={item.host.picture} alt="hôte" />
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,23 @@
-import data from '../data.json'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 export default function Card() {
-  const accomodationList = data
+  const [accomodationList, setAccomodationList] = useState([])
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((data) => setAccomodationList(data))
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des données:', error)
+      })
+  }, [])
   return (
     <div className="cards__container">
       {accomodationList.map((item, index) => (
         <Link
           className="cards__container--link"
           to={`/accomodation/${item.id}`}
+          key={item.id}
         >
           <div className="cards__container--card" key={index.id}>
             <img
